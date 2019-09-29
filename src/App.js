d74cc691a8810af7caccd081430f76e6d9064218
Noman <nomankurkur95@gmail.com>
2019-09-29 14:45:19 +0500

    Sign UP

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/navbar';
+import { BrowserRouter as Router, Route } from
+  "react-router-dom";
+import SignIn from './components/signin';
+import SignUp from './components/signup';
+import { MDBRow, MDBContainer, MDBCol } from 'mdbreact'
+import {  makeStyles } from '@material-ui/core/styles';
+
+function App() {
+
+  const useStyles = () => makeStyles(theme => {
+    
+    // topMarging:{
+    //   margin:50;
+    // }
+  });
+
+  const classes = useStyles();
+
+
+  return (
+    <div >
+      <Router>
+        {<NavBar />}
+        {/* <SignUp/> */}
+        <MDBContainer style={{marginTop:'70px'}}>
+        
+          <MDBRow>
+            <MDBCol size="4"></MDBCol>
+            <MDBCol size="4">
+              <Route path="/signin" component={SignIn} />
+              <Route path="/signup" component={SignUp} />
+            </MDBCol>
+            <MDBCol size="4"></MDBCol>
+
+          </MDBRow>
+        </MDBContainer>
+
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/signup.js
@@ -0,0 +1,86 @@
+import React from "react";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBModalFooter,
+  MDBIcon,
+  MDBCardHeader,
+  MDBBtn,
+  MDBInput
+} from "mdbreact";
+
+const SignUp = () => {
+  return (
+    <MDBContainer>
+      <MDBRow>
+        <MDBCol md="12">
+          <MDBCard>
+            <MDBCardBody>
+              <MDBCardHeader className="form-header deep-blue-gradient rounded">
+                <h3 className="my-3">
+                  <MDBIcon icon="lock" /> Sign Up:
+                </h3>
+              </MDBCardHeader>
+              <form>
+                <div className="grey-text">
+                <MDBInput
+                label="Your name"
+                icon="user"
+                group
+                type="text"
+                validate
+                error="wrong"
+                success="right"
+              />
+              <MDBInput
+                label="Your email"
+                icon="envelope"
+                group
+                type="email"
+                validate
+                error="wrong"
+                success="right"
+              />
+              <MDBInput
+                label="Confirm your email"
+                icon="exclamation-triangle"
+                group
+                type="text"
+                validate
+                error="wrong"
+                success="right"
+              />
+              <MDBInput
+                label="Your password"
+                icon="lock"
+                group
+                type="password"
+                validate
+              />
+                </div>
+
+              <div className="text-center mt-4">
+                <MDBBtn
+                  color="light-blue"
+                  className="mb-3"
+                  type="submit"
+                >
+                  Sign Up
+                </MDBBtn>
+              </div>
+              </form>
+              <MDBModalFooter>
+               
+              </MDBModalFooter>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+};
+
+export default SignUp;
